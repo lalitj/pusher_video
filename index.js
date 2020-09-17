85235226af299dc8f4e6e5f9687f5f8788ac5f62
Lalit J <lalit@extreme-agile.com>
2020-09-17 18:39:28 +0530

    Keys added.

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create an instance of Pusher
 const pusher = new Pusher({
-    appId: 'XXX-APP-ID',
-    key: 'XXX-APP-KEY',
-    secret: 'XXX-APP-SECRET',
-    cluster: 'XXX-APP-CLUSTER',
+    appId: '924973',
+    key: 'd7d3cfba04f21c254eec',
+    secret: '0d5610c9230cdff92793',
+    cluster: 'ap2',
     encrypted: true
 });
 
@@ -26,13 +26,13 @@ app.get('/', (req, res) => {
 
 // get authentictation for the channel;
 app.post('/pusher/auth', (req, res) => {
-        const socketId = req.body.socket_id;
-        const channel = req.body.channel_name;
-        var presenceData = {
-            user_id: Math.random().toString(36).slice(2) + Date.now()
-        }
-        const auth = pusher.authenticate(socketId, channel, presenceData);
-        res.send(auth);
+    const socketId = req.body.socket_id;
+    const channel = req.body.channel_name;
+    var presenceData = {
+        user_id: Math.random().toString(36).slice(2) + Date.now()
+    }
+    const auth = pusher.authenticate(socketId, channel, presenceData);
+    res.send(auth);
 });
 
 //listen on the app
